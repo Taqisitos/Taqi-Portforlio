f9e9b7acbe5bfe44640d751dbaf8aa91f71743de
Taqi Hossain <taqi.hossain@gmail.com>
2022-11-16 16:16:27 -0600

    react app and navbar

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import ColorSchemesExample from "./Navbar.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (<ColorSchemesExample></ColorSchemesExample>);
+}
+
+export default App;
---
+++ src/Intro.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+function Intro() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Intro;
